3107d8988fd84303bb7b1b465620e71a0c5fc40f
Shaan <shaanalam369@gmail.com>
2023-08-20 13:15:38 +0530

    fixed note context

--- components/NoteContext.tsx
+++ components/NoteContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { NoteInterface } from "@/api/collection";
 
 export interface IContextType {
