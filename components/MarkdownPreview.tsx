d0571947f5fbcb8944b4c106ebc40368ca5fa3d8
Shaan <shaanalam369@gmail.com>
2023-08-20 11:50:29 +0530

    fix: markdown preview styles

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -9,7 +9,7 @@ interface MarkdownPreviewProps {
 
 const MarkdownPreview: React.FC<MarkdownPreviewProps> = ({ title, body }) => {
   return (
-    <div className="markdown-preview px-4 py-2 overflow-auto leading-7 [&>*]:my-4 [&>h1]:text-6xl [&>h2]:text-5xl [&>h3]:text-4xl [&>h4]:text-3xl [&>h5]:text-3xl [&>h6]:text-2xl [&>h1]:my-6 [&>h2]:my-6 [&>h3]:my-6 [&>h4]:my-6 [&>h5]:my-6 [&>h6]:my-6 [&>h1]:font-bold [&>h2]:font-bold [&>h3]:font-bold [&>h4]:font-bold [&>h5]:font-bold [&>h6]:font-bold  dark:[&>*]:text-white">
+    <div className="markdown-preview px-8 py-2 overflow-auto leading-7 [&>*]:my-4 [&>h1]:text-6xl [&>h2]:text-5xl [&>h3]:text-4xl [&>h4]:text-3xl [&>h5]:text-3xl [&>h6]:text-2xl [&>h1]:my-6 [&>h2]:my-6 [&>h3]:my-6 [&>h4]:my-6 [&>h5]:my-6 [&>h6]:my-6 [&>h1]:font-bold [&>h2]:font-bold [&>h3]:font-bold [&>h4]:font-bold [&>h5]:font-bold [&>h6]:font-bold  dark:[&>*]:text-white">
       <h6>{title}</h6>
       {/* @ts-ignore */}
       <ReactMarkdown
